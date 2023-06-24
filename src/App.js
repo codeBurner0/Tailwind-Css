24909b9f8540d9f0e99d8433bd399b6c37129286
CodeBurner <codeburner0@gmail.com>
2023-06-24 15:37:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Color from './Topics/Color'
+function App() {
+  return (
+    <div>
+      < Color/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Topics/Color.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function color() {
+  return (
+    <div>
+      <h1 className=' text-purple-900 font-bold mx-5 my-3'>Name: Ankit Anand</h1>
+      <h1 className=' text-red-900 font-semibold mx-5'>Section: D</h1>
+      <div className=' bg-purple-950 hover:bg-fuchsia-500 w-5 h-5 m-7'></div>
+      <div className=' bg-red-700 hover:bg-yellow-600 w-5 h-5 m-7'></div>
+      </div>
+    
+  )
+}
+
+export default color
