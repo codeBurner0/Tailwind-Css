cd80082f51d251dc4003baafa7d677340ae2287c
CodeBurner <codeburner0@gmail.com>
2023-06-26 07:19:40 +0530

    updated

--- src/Topics/Login.js
+++ src/Topics/Login.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function Login() {
   return (
-    <div>
-      <div className='w-1/3 h-96 space-x-96 my-36 bg-black rounded-xl shadow-2xl shadow-black text-center justify-items-end justify-items-center'>
-        <h1 className=' text-white'>Login</h1>
-      </div>
+    <div className=" border border-4 border-gray-600 border-dotted relative w-1/3 h-96 my-36 bg-black rounded-xl shadow-2xl shadow-black m-[35%]">
+      <h1 className=" text-white text-center">Login</h1>
+      <button className=" absolute bottom-5 left-[39%] border border-blue-950 border-4 bg-blue-400 p-2 px-4 text-xl font-bold rounded-xl">
+        Submit
+      </button>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
