b86cdd0612faa6ae9a0ce899744b52d8b9778ada
CodeBurner <codeburner0@gmail.com>
2023-06-26 07:40:43 +0530

    background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 // import Color from './Topics/Color'
 // import Login from './Topics/Login'
-import Typography from './Topics/Typography'
+// import Typography from './Topics/Typography'
+import BackGround from './Topics/BackGround'
 function App() {
   return (
     <div>
-      < Typography/>
+      < BackGround/>
     </div>
   )
 }
---
+++ src/Topics/BackGround.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function BackGround() {
+  return (
+    <div>
+      <h1 className=" font-bold text-xl underline decoration-amber-600">
+        BackGroound Styling in Tailwind CSS
+      </h1>
+    </div>
+  );
+}
+
+export default BackGround;
