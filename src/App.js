000a72824eccec2f7063489a3a5eb4395b9532a7
CodeBurner <codeburner0@gmail.com>
2023-06-27 12:42:23 +0530

    updtaed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 // import Color from './Topics/Color'
-// import Login from './Topics/Login'
+import Login from './Topics/Login'
 // import Typography from './Topics/Typography'
-import BackGround from './Topics/BackGround'
+// import BackGround from './Topics/BackGround'
 function App() {
   return (
     <div>
-      < BackGround/>
+      < Login/>
     </div>
   )
 }
--- src/Topics/Login.js
+++ src/Topics/Login.js
@@ -2,9 +2,20 @@ import React from "react";
 
 function Login() {
   return (
-    <div className=" border border-4 border-gray-600 border-dotted relative w-1/3 h-96 my-36 bg-black rounded-xl shadow-2xl shadow-black m-[35%]">
-      <h1 className=" text-white text-center">Login</h1>
-      <button className=" absolute bottom-5 left-[39%] border border-blue-950 border-4 bg-blue-400 p-2 px-4 text-xl font-bold rounded-xl">
+    <div className=" relative w-1/3 h-96 my-36 bg-black rounded-xl shadow-2xl shadow-black m-[35%]">
+      <h1 className=" text-white text-center font-bold text-3xl relative top-10 font-mono">
+        Login
+      </h1>
+      <h1 className=" text-[aqua] relative top-12 left-14 text">UserName</h1>
+      <input
+        type="text"
+        className=" border-b-white absolute bg-black border-b-4 top-28 left-14 w-[70%]"
+      />
+      <input
+        type="text"
+        className=" border-b border-b-white bg-black border-b-4 absolute top-40 left-14 w-[70%]"
+      />
+      <button className=" absolute bottom-5 ml-9  border border-blue-950 border-4 bg-blue-400 p-2 px-4 text-xl font-bold rounded-xl">
         Submit
       </button>
     </div>
